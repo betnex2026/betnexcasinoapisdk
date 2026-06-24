2277947d066e1e88bebe1fc4932e2e01b0c33a83
Jayesh10112002 <info.jayeshvijay@gmail.com>
2026-06-24 08:34:09 +0530

    update

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ export { calculateBalance } from "./utils/calculateBalance.js";
 export { createCallbackResponse } from "./utils/createCallbackResponse.js";
 
 // SDK Version
-export const VERSION = "1.0.0";
+export const VERSION = "1.0.4";
