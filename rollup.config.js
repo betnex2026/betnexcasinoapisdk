46e88a371695ea593948f4cc001f448d638bd888
Jayesh10112002 <info.jayeshvijay@gmail.com>
2026-06-24 08:22:32 +0530

    SDK Updated

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), terser()],
+    external: ["axios"],
+  },
+];
